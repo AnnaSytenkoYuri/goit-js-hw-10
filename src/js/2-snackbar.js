4f282b14ebfeba677499db634d5455069e0b2b7c
Anna Sytenko <anna_sytenko91@icloud.com>
2025-07-13 00:22:40 +0700

    second task added

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,42 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+
+  createPromises(state, delay)
+    .then(delay => {
+      iziToast.success({
+        title: '✅ Success',
+        message: `Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: '❌ Error',
+        message: `Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
+
+
+function createPromises(state, delay) {
+    return new Promise((resolve, reject) => {
+        
+        setTimeout(() => {
+            if (state === 'fulfilled') {
+               resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay)
+       
+    });
+}
